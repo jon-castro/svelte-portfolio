754b6c294b17f2b4f5db4e90d38877631a7df625
Jonathan Castro <23022436+jon-castro@users.noreply.github.com>
2020-03-02 21:57:25 -0500

    Adjusting About component.

--- portfolio/src/Components/About.svelte
+++ portfolio/src/Components/About.svelte
@@ -2,9 +2,9 @@
 
 </script>
 
-<div class="bg-blue-900">
-  <h1 class="p-8 text-center sm:text-left text-gray-400">Jonathan Castro</h1>
-  <h2 class="pl-8 pb-10 text-center sm:text-left text-gray-4S00">
-    web developer and software quality assurance professional
+<div>
+  <h1 class="p-8 text-center text-light-tan">Jonathan Castro</h1>
+  <h2 class="pl-8 pb-10 text-center text-light-tan">
+    Web development and software quality assurance professional
   </h2>
 </div>
