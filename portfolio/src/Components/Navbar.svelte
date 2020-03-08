d7b9552bb5b1753de5bdb0e4f808d9235c951aec
Jonathan Castro <23022436+jon-castro@users.noreply.github.com>
2020-03-07 19:02:42 -0500

    Testing changes to navbar.

--- portfolio/src/Components/Navbar.svelte
+++ portfolio/src/Components/Navbar.svelte
@@ -4,23 +4,69 @@
 </script>
 
 <style>
-  header {
-    background-image: url("/images/navbar_bg.png");
-  }
   .link {
     @apply text-light-gray;
   }
 </style>
 
-<header class="bg-dark-blue bg-center">
+<header class="bg-indigo-900">
   <div class="flex items-center justify-between px-4 py-3">
     <div>
       <img
-        class="h-10"
+        class="h-8 sm:block"
         src="/images/jcastro.dev_text_logo.png"
         alt="Welcome to Jonathan Castro's page!" />
     </div>
-    <div class="sm:hidden">
+    <!-- <div class="sm:hidden {isMenuOpen ? 'justify-end' : 'justify-between'}">
+      <button
+        type="button"
+        class="block text-gray-500 hover:text-white focus:text-white
+        focus:outline-none"
+        on:click={toggleMenu}>
+        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+          {#if isMenuOpen}
+            <path
+              fill-rule="evenodd"
+              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1
+              1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1
+              1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828
+              4.829 4.828 4.828z" />
+          {:else}
+            <path
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1
+              1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+          {/if}
+        </svg>
+      </button>
+    </div> -->
+    <!-- </div>
+  <div class="sm:flex sm:justify-evenly"> -->
+    <a
+      href="/"
+      class="mt-1 px-2 py-3 text-gray font-semibold rounded hover:bg-elements {isMenuOpen ? 'block bg-red-800 ml-8 mt-1 mb-0' : 'hidden'}
+      sm:inline-block sm:justify-between link">
+      Home
+    </a>
+    <a
+      href="#projects"
+      class="mt-1 px-2 py-3 text-white font-semibold rounded hover:bg-elements {isMenuOpen ? 'block' : 'hidden'}
+      sm:inline-block sm:justify-between link">
+      Projects
+    </a>
+    <a
+      href="#articles"
+      class="mt-1 px-2 py-3 text-white font-semibold rounded hover:bg-elements {isMenuOpen ? 'block' : 'hidden'}
+      sm:inline-block sm:justify-between link">
+      Articles
+    </a>
+    <a
+      href="#contact"
+      class="mt-1 px-2 py-3 text-white font-semibold rounded hover:bg-elements {isMenuOpen ? 'block' : 'hidden'}
+      sm:inline-block sm:justify-between link">
+      Contact
+    </a>
+    <div class="sm:hidden {isMenuOpen ? 'justify-end' : 'justify-between'}">
       <button
         type="button"
         class="block text-gray-500 hover:text-white focus:text-white
@@ -43,31 +89,5 @@
         </svg>
       </button>
     </div>
-  </div>
-  <div class="sm:flex sm:justify-evenly">
-    <a
-      href="/"
-      class="mt-1 px-2 py-3 text-gray font-semibold rounded hover:bg-blue-800 {isMenuOpen ? 'block' : 'hidden'}
-      sm:inline-block sm:justify-between link">
-      Home
-    </a>
-    <a
-      href="#projects"
-      class="mt-1 px-2 py-3 text-white font-semibold rounded hover:bg-blue-800 {isMenuOpen ? 'block justify-center' : 'hidden'}
-      sm:inline-block sm:justify-between link">
-      Projects
-    </a>
-    <a
-      href="#articles"
-      class="mt-1 px-2 py-3 text-white font-semibold rounded hover:bg-blue-800 {isMenuOpen ? 'block' : 'hidden'}
-      sm:inline-block sm:justify-between link">
-      Articles
-    </a>
-    <a
-      href="#contact"
-      class="mt-1 px-2 py-3 text-white font-semibold rounded hover:bg-blue-800 {isMenuOpen ? 'block' : 'hidden'}
-      sm:inline-block sm:justify-between link">
-      Contact
-    </a>
   </div>
 </header>
--- portfolio/src/data/articles.js
+++ portfolio/src/data/articles.js
@@ -2,23 +2,29 @@ let articles = [
   {
     id: 0,
     title: "Article title 1",
-    summary: "Article summary 1",
-    body: "Article body 1",
-    image: "imageLocationHere"
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo et nulla pharetra accumsan.",
+    body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo et nulla pharetra accumsan. Donec vehicula varius eros, ac dapibus massa tincidunt id. Duis consectetur ex id leo varius malesuada. Nam malesuada tincidunt mi quis commodo. Nulla vitae tempus urna, a ultricies magna. Pellentesque posuere, quam a sagittis tristique, mi nulla fermentum turpis, id bibendum mi nisl sed nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus tristique magna vel mollis. Vivamus sit amet nunc eu dui vehicula sollicitudin quis vitae ipsum. Nunc eu nisi a ex tristique molestie. Mauris a varius risus. Duis venenatis, lacus eget sagittis facilisis, ex ligula fermentum turpis, in lobortis enim eros eu mi.",
+    image: "https://loremflickr.com/600/480/"
   },
   {
     id: 1,
     title: "Article title 2",
-    summary: "Article summary 2",
-    body: "Article body 2",
-    image: "imageLocationHere"
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo et nulla pharetra accumsan.",
+    body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo et nulla pharetra accumsan. Donec vehicula varius eros, ac dapibus massa tincidunt id. Duis consectetur ex id leo varius malesuada. Nam malesuada tincidunt mi quis commodo. Nulla vitae tempus urna, a ultricies magna. Pellentesque posuere, quam a sagittis tristique, mi nulla fermentum turpis, id bibendum mi nisl sed nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus tristique magna vel mollis. Vivamus sit amet nunc eu dui vehicula sollicitudin quis vitae ipsum. Nunc eu nisi a ex tristique molestie. Mauris a varius risus. Duis venenatis, lacus eget sagittis facilisis, ex ligula fermentum turpis, in lobortis enim eros eu mi.",
+    image: "https://loremflickr.com/600/480/"
   },
   {
     id: 2,
     title: "Article title 3",
-    summary: "Article summary 3",
-    body: "Article body 3",
-    image: "imageLocationHere"
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo et nulla pharetra accumsan.",
+    body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel leo et nulla pharetra accumsan. Donec vehicula varius eros, ac dapibus massa tincidunt id. Duis consectetur ex id leo varius malesuada. Nam malesuada tincidunt mi quis commodo. Nulla vitae tempus urna, a ultricies magna. Pellentesque posuere, quam a sagittis tristique, mi nulla fermentum turpis, id bibendum mi nisl sed nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus tristique magna vel mollis. Vivamus sit amet nunc eu dui vehicula sollicitudin quis vitae ipsum. Nunc eu nisi a ex tristique molestie. Mauris a varius risus. Duis venenatis, lacus eget sagittis facilisis, ex ligula fermentum turpis, in lobortis enim eros eu mi.",
+    image: "https://loremflickr.com/600/480/"
   }
 ];
 
