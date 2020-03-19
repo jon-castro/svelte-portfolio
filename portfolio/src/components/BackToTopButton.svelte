abcc3ab0fecca41322c1546c281801c2ba0a8c42
Jonathan Castro <jcastrogarcia@gmail.com>
2020-03-18 23:04:29 -0400

    Back to top button implemented.

--- portfolio/src/components/BackToTopButton.svelte
+++ portfolio/src/components/BackToTopButton.svelte
@@ -2,24 +2,10 @@
   import { isDarkMode } from "../stores.js";
 </script>
 
-<style>
-  #myBtn {
-    position: fixed; /* Fixed/sticky position */
-    bottom: 20px; /* Place the button at the bottom of the page */
-    right: 30px; /* Place the button 30px from the right */
-    z-index: 99; /* Make sure it does not overlap */
-    border: none; /* Remove borders */
-    outline: none; /* Remove outline */
-    cursor: pointer; /* Add a mouse pointer on hover */
-    padding: 15px; /* Some padding */
-    border-radius: 10px; /* Rounded corners */
-  }
-</style>
-
 <a href="#header">
   <button
-    id="myBtn"
-    class="h-10 w-10 bg-red-700 hover:bg-indigo-800 outline-none {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    class="h-12 w-12 bg-red-700 border-none cursor-pointer fixed bottom-0
+    right-0 outline-none">
     Top
   </button>
 </a>
