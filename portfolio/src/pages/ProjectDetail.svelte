8f108a892fa51fec51dea2375ae18e528a633883
Jonathan Castro <jcastrogarcia@gmail.com>
2020-03-21 19:01:05 -0400

    Work on About section.

--- portfolio/src/pages/ProjectDetail.svelte
+++ portfolio/src/pages/ProjectDetail.svelte
@@ -51,24 +51,6 @@
       <DarkModeButton />
     </div>
   </div>
-  <!-- <div class="grid grid-flow-row grid-cols-6 grid-rows-1">
-    <div class="col-start-2 col-span-2 pt-3">
-      <a href="/">
-        <button
-          class="{$isDarkMode ? 'bg-section-dark' : 'bg-section-light'}
-          text-text-buttons font-bold py-2 px-4 mt-2 ml-2 border-b-4
-          border-indigo-700 rounded">
-          <div class="icon inline-block">
-            <FaArrowLeft />
-          </div>
-          Back to Projects List
-        </button>
-      </a>
-    </div>
-    <div class="col-start-6 pt-4">
-      <DarkModeButton />
-    </div>
-  </div> -->
   <h1
     class="pb-2 text-center opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
     {data.title}
