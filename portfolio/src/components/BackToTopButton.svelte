0a0292f488a1cd33ed7b2031f588ea576a4d0094
Jonathan Castro <jcastrogarcia@gmail.com>
2020-03-22 21:31:12 -0400

    Finished skills modal content.

--- portfolio/src/components/BackToTopButton.svelte
+++ portfolio/src/components/BackToTopButton.svelte
@@ -4,15 +4,6 @@
   import { isDarkMode } from "../stores.js";
 </script>
 
-<!-- Original button -->
-<!-- <a href="#header">
-  <button
-    class="h-12 w-12 bg-red-700 opacity-50 sm:opacity-25 md:hover:opacity-100
-    border-none cursor-pointer fixed bottom-0 right-0 outline-none">
-    Top
-  </button>
-</a> -->
-
 <a
   href="#header"
   class="icon h-12 w-12 text-red-700 opacity-50 sm:opacity-25
--- portfolio/src/components/SkillsContent.svelte
+++ portfolio/src/components/SkillsContent.svelte
@@ -2,10 +2,160 @@
   import { isDarkMode } from "../stores.js";
 </script>
 
-<style>
-
-</style>
-
-<h1 class="opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
-  Title placed locally
+<h1
+  class="text-center opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+  Skills
 </h1>
+<hr
+  class="mt-0 mx-12 pb-10 border-t-2 opacity-75 {$isDarkMode ? 'border-text-dark' : 'border-text-light'}" />
+<h2 class="opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+  Web Development
+</h2>
+<ul class="opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    HTML/CSS/Javascript
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Node.js
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Svelte.js
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    React
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Tailwind CSS
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Python
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Django
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Django REST Framework
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    C#
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    .NET Core 3
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Dart
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Flutter
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Databases
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    PostgreSQL
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Oracle
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Microsoft SQL Server
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    mongoDB
+  </li>
+</ul>
+<h2 class="opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+  Software Quality Assurance
+</h2>
+<ul class="opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Selenium WebDriver
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Python & C# bindings
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Test Case Management
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Azure DevOps
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    HP Quality Center
+  </li>
+</ul>
+<h2 class="opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+  Tech Industry
+</h2>
+<ul>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    IaaS
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    AWS Lightsail, S3, Workmail, Route 53
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    PaaS
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Heroku
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Scrum/Project Management
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    JIRA
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Trello
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Notion
+  </li>
+  <li
+    class="text-center font-bold opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Operating Systems
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Apple macOS
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Linux (Ubuntu, Red Hat (RHEL), CentOS)
+  </li>
+  <li
+    class="text-center font-light opacity-75 {$isDarkMode ? 'text-text-dark' : 'text-text-light'}">
+    Microsoft Windows Server
+  </li>
+</ul>
