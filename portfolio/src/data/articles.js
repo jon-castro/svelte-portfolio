ec14224ad9e2f07a2029418337be6757d6b66554
Jonathan Castro <jcastrogarcia@gmail.com>
2020-03-25 23:07:56 -0400

    Finished formatting for long form text content.

--- portfolio/src/data/articles.js
+++ portfolio/src/data/articles.js
@@ -5,7 +5,7 @@ let articles = [
       "Making a portfolio using Svelte, Tailwind CSS, and a few easy tricks.",
     summary: "A quick write-up how I made this website, concept to content.",
     body: `
-    <p>Working on the how-to of the articles.</p>
+    <p>Article coming soon...</p>
     `,
     image: "/images/neon-keyboard-with-enter-button-focus-on-the-P6GQUWM.jpg"
   }
--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -3,8 +3,9 @@ let projects = [
     id: 0,
     title: "jcastro.dev (this website)",
     summary: "A mobile first portfolio mainly using Svelte and Tailwind CSS",
-    body:
-      "This very same website, jcastro.dev, was built as a personal project to host links to my work and articles on topics relevant to my professional pursuits. It uses the Svelte framework (https://svelte.dev/) to fetch and show content, do routing, and hold all the logic for all of it. The Tailwind CSS framework (https://tailwindcss.com/) is used for most of the styling within. The 'svelte-simple-modal' repo (https://github.com/flekschas/svelte-simple-modal) was adapted for the site to be able to change based on the color schemes.",
+    body: `
+    <span>This very same website, </span><a class="text-indigo-600" href="https://jcastro.dev" target="_blank">jcastro.dev</a><span>, was built as a personal project to host links to my work and articles. It uses the </span><a class="text-indigo-600" href="https://svelte.dev/" target="_blank">Svelte</a><span> framework to fetch and show content, perform routing, and work out all logic. The </span><a class="text-indigo-600" href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a><span> framework is used for most of the styling. The </span><a class="text-indigo-600" href="https://github.com/flekschas/svelte-simple-modal" target="_blank">svelte-simple-modal</a><span> repo was adapted for the site to be able to change based on the color schemes.</span>
+    `,
     repoUrl: "https://github.com/jon-castro/svelte-portfolio",
     demoUrl: "",
     image: "/images/portfolio.png"
