410ac9a8a0e3efedc2a65ffb6eba851cdc41cdfa
Jonathan Castro <jcastrogarcia@gmail.com>
2020-03-26 23:48:53 -0400

    Preparation to deploy.

--- portfolio/src/data/articles.js
+++ portfolio/src/data/articles.js
@@ -3,7 +3,8 @@ let articles = [
     id: 0,
     title:
       "Making a portfolio using Svelte, Tailwind CSS, and a few easy tricks.",
-    summary: "A quick write-up how I made this website, concept to content.",
+    summary:
+      "(WIP) A quick write-up how I made this website, concept to content.",
     body: `
     <p>Article coming soon...</p>
     `,
