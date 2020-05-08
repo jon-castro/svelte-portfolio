ebba7a032cd78627f73dad369084a834dec795c1
Jonathan Castro <jcastrogarcia@gmail.com>
2020-05-08 18:23:31 -0400

    Added .NET Core 3 Selenium project.

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -1,17 +1,6 @@
 let projects = [
   {
     id: 0,
-    title: "jcastro.dev (this website)",
-    summary: "A mobile first portfolio mainly using Svelte and Tailwind CSS",
-    body: `
-    <span>This very same website, </span><a class="text-indigo-600" href="https://jcastro.dev" target="_blank"><u>jcastro.dev</u></a><span>, was built as a personal project to host links to my work and articles. It uses the </span><a class="text-indigo-600" href="https://svelte.dev/" target="_blank"><u>Svelte</u></a><span> framework to fetch and show content, perform routing, and work out all logic. The </span><a class="text-indigo-600" href="https://tailwindcss.com/" target="_blank"><u>Tailwind CSS</u></a><span> framework is used for most of the styling. The </span><a class="text-indigo-600" href="https://github.com/flekschas/svelte-simple-modal" target="_blank"><u>svelte-simple-modal</u></a><span> repo was adapted for the site to be able to change based on the color schemes.</span>
-    `,
-    repoUrl: "https://github.com/jon-castro/svelte-portfolio",
-    demoUrl: "",
-    image: "/images/portfolio.png"
-  },
-  {
-    id: 1,
     title: ".NET Core 3 - UI Test Suite",
     summary: "Testing Suite for showcasing UI automation processes using the demo web app at Swag Labs.",
     body: `
@@ -21,6 +10,17 @@ let projects = [
     `,
     repoUrl: "",
     demoUrl: "https://dev.azure.com/jcastrogarcia/Selenium%20UI%20test%20suite%20-%20.NET%20Core%203/_git/saucedemo-ui-test-suite",
+    image: "/images/ui_test_suite_csharp.png"
+  },
+  {
+    id: 1,
+    title: "jcastro.dev (this website)",
+    summary: "A mobile first portfolio mainly using Svelte and Tailwind CSS",
+    body: `
+    <span>This very same website, </span><a class="text-indigo-600" href="https://jcastro.dev" target="_blank"><u>jcastro.dev</u></a><span>, was built as a personal project to host links to my work and articles. It uses the </span><a class="text-indigo-600" href="https://svelte.dev/" target="_blank"><u>Svelte</u></a><span> framework to fetch and show content, perform routing, and work out all logic. The </span><a class="text-indigo-600" href="https://tailwindcss.com/" target="_blank"><u>Tailwind CSS</u></a><span> framework is used for most of the styling. The </span><a class="text-indigo-600" href="https://github.com/flekschas/svelte-simple-modal" target="_blank"><u>svelte-simple-modal</u></a><span> repo was adapted for the site to be able to change based on the color schemes.</span>
+    `,
+    repoUrl: "https://github.com/jon-castro/svelte-portfolio",
+    demoUrl: "",
     image: "/images/portfolio.png"
   }
 ];
